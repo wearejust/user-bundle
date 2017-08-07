60d0ce716bb1f3888dab0c533f2b4128801ef079
Cees van Egmond <ceesvanegmond@users.noreply.github.com>
2017-08-07 16:44:12 +0200

    Update GroupAdmin.php

--- Admin/GroupAdmin.php
+++ Admin/GroupAdmin.php
@@ -3,9 +3,35 @@
 namespace Wearejust\UserBundle\Admin;
 
 use Sonata\UserBundle\Admin\Entity\GroupAdmin as BaseGroupAdmin;
-use Wearejust\AdminBundle\Admin\Traits\DisableDefaults;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 
 class GroupAdmin extends BaseGroupAdmin
 {
-    use DisableDefaults;
+    /**
+     * Disable all export formats by default, off course you can override this
+     * action when needed.
+     *
+     * https://sonata-project.org/bundles/admin/master/doc/reference/action_export.html
+     *
+     * @return array
+     */
+    public function getExportFormats()
+    {
+        return [];
+    }
+
+    /**
+     * Disable the filters on the list page by default, off course you can enable
+     * it when needed.
+     *
+     * https://sonata-project.org/bundles/admin/master/doc/reference/action_list.html
+     *
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $filterMapper
+     *
+     * @return \Sonata\AdminBundle\Datagrid\DatagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $filterMapper)
+    {
+        return $filterMapper;
+    }
 }
